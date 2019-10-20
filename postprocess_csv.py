5b01f2bdda7d3fefa003031791201da43ad8592b
chewarer <chewarer@yandex.ru>
2019-10-20 14:45:52 +0200

    Rebuild CSV with additional information.

---
+++ postprocess_csv.py
@@ -0,0 +1,54 @@
+import json
+from itertools import chain
+
+from parser_lib import read_csv, save_csv, flat_params, map_params, flat_prod_params
+
+
+def get_subkeys_by(key: str, file: str):
+    # file = 'data/items.csv'
+    data = read_csv(file)
+    all_keys = (json.loads(x.get(key)).keys() for x in data)
+    all_keys = set(chain(*all_keys))
+
+    return all_keys
+
+
+def main():
+    """Rebuild CSV with additional information"""
+
+    fname_new = 'data/items_ext.csv' \
+                ''
+    rows = read_csv('data/items.csv')
+    mapper = map_params()
+    all_keys = list(rows[0].keys())
+
+    # Set category name by last breadcrumb element
+    all_keys.extend((*flat_prod_params(), 'category_name', *flat_params()))
+
+    for row in rows:
+        # Set default value for all keys
+        for k in all_keys:
+            row.setdefault(k, '')
+
+        # Set key params to columns specified for parent category
+        row['category_name'] = row.get('breadcrumb_bottom', '').split('|')[-1]
+        key = row.get('breadcrumb_urls', '').split(',')[-1]
+        keys = mapper.get(key, {}).values()
+        keys = set(*keys) if keys else set()
+        keys.discard('Цена')
+        params = json.loads(row.get('tech_params', '')) or {}
+
+        # Set key parameters to column
+        for k in keys:
+            row[k] = params.get(k)
+
+        #  Set prodict param to columns
+        for k, v in json.loads(row.get('prodict_param', {})).items():
+            row[k] = v
+
+    # Write new csv
+    save_csv(rows, fname_new, update=False)
+
+
+if __name__ == '__main__':
+    main()
